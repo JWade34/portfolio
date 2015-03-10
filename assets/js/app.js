080ec54d2af85f754b75557033402672e4a14561
Justin <justinmckelvey@hotmail.com>
2015-03-10 16:06:34 -0500

    scroll tabs

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,35 +1,35 @@
-$(document).ready(function(){
-
-//1st: select the element that will 'receive' an event
-  $('.emailme').closest('form')
-  //2nd: determine the event listened for (i.e. click, dblclick, submit....)
-    .on('submit',
-    //3rd do shit
-        function(evt){
-
-          evt.preventDefault();
-
-          var nameInput = $('#name').val();
-          var emailInput = //...
-          console.log(nameInput)
-
-          $('#name').val('')
-
-  })
-
-    $('.emailme').closest('form').on('submit',function(evt){
-
-      evt.preventDefault();
-
-      var nameInput = $('#name').val();
-
-      console.log(nameInput)
-
-      $('#name').val('')
-
-    })
-
-
+// $(document).ready(function(){
+//
+// //1st: select the element that will 'receive' an event
+//   $('.emailme').closest('form')
+//   //2nd: determine the event listened for (i.e. click, dblclick, submit....)
+//     .on('submit',
+//     //3rd do shit
+//         function(evt){
+//
+//           evt.preventDefault();
+//
+//           var nameInput = $('#name').val();
+//           var emailInput = //...
+//           console.log(nameInput)
+//
+//           $('#name').val('')
+//
+//   })
+//
+//     $('.emailme').closest('form').on('submit',function(evt){
+//
+//       evt.preventDefault();
+//
+//       var nameInput = $('#name').val();
+//
+//       console.log(nameInput)
+//
+//       $('#name').val('')
+//
+//     })
+//
+//
 
 
 
